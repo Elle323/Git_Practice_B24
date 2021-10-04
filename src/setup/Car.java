13dac1c8c2fc1c3bc7d8982d9b7e1fe66699e761
Elle323 <ellieamineva@gmail.com>
2021-10-04 19:00:24 -0400

    added wooden spoon test

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -1,11 +1,9 @@
 package setup;
 
 public class Car {
-<<<<<<< HEAD
     //this is car class from master branch
     //different text
-=======
     //this is our car from james branch
     //more text
->>>>>>> James
+
 }
