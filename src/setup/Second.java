34bcb4e55a4b3ef911eb7917d45ba631f2e9b9fd
Elle323 <ellieamineva@gmail.com>
2021-10-04 17:22:01 -0400

    Updated Second file

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -3,5 +3,7 @@ package setup;
 public class Second {
     public static void main(String[] args) {
         System.out.println("Something");
+
+        System.out.println("Added more lines");
     }
 }
